ba5e55d1ba7c33a7cc7b7486af6204bc2c2e35e0
Wonderlus <iliyawhitelion@gmail.com>
2022-12-28 13:07:43 +0300

    Marico

---
+++ Marico/js/app.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    let header = $("#header");
+    let headerHeight = $("#header").innerHeight();
+    let scrollOffset = $(window).scrollTop();
+
+    CheckScroll(headerHeight, scrollOffset);
+
+
+    // Toggle Burger
+    $("#navToggle").on("click", function(event) {
+        event.preventDefault();
+        
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+
+    // Fixed Header
+    $(window).on("scroll", function() {
+
+        let scrollOffset = $(this).scrollTop();
+        CheckScroll(headerHeight, scrollOffset);
+
+
+    });
+
+    function CheckScroll(headerHeight, scrollOffset) {
+        
+        if (headerHeight < scrollOffset) {
+            $("#header").addClass("fixed");
+        } else {
+            $("#header").removeClass("fixed");
+        }
+    }
+
+
+    // Smooth scroll
+    $(".logoHome").on("click", function() {
+
+        $("html, body").animate({
+            scrollTop: 0,
+        });
+
+    });
+})
